fb1dd38c1d9e5e2dcffa72c709dc1327549ebc1e
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2024-06-25 14:12:13 +0300

    html template created render added to views

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 
-# Create your views here.
+
+
 def index(request):
-    return HttpResponse('Hello from the Shop app')
+    courses = Course.objects.all()
+    # return HttpResponse(courses)
+    return render(request, 'courses.html')
+
+
