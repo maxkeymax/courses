d3c1f8e4289fbc7be784b398feff586697c15911
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2024-06-29 15:54:33 +0300

    single_course.html added
     according changes to urls, views added

--- shop/views.py
+++ shop/views.py
@@ -9,4 +9,6 @@ def index(request):
     # return HttpResponse(courses)
     return render(request, 'courses.html', {'courses': courses})
 
-
+def single_course(request, course_id):
+    course = Course.objects.get(pk=course_id)
+    return render(request, 'single_course.html', {'course': course})
