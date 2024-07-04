c3ba6fed8b581245bffac895c1f861b25139a051
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2024-07-04 11:55:53 +0300

    App shop completed
    Shop templates moved to the root dir and separated between the corresponding folders

--- shop/views.py
+++ shop/views.py
@@ -7,19 +7,19 @@ from .models import Course
 def index(request):
     courses = Course.objects.all()
     # return HttpResponse(courses)
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
 
 def single_course(request, course_id):
     # Option 1
     # try:
     #     course = Course.objects.get(pk=course_id)
-    #     return render(request, 'single_course.html', {'course': course})
+    #     return render(request, 'shop/single_course.html', {'course': course})
     # except Course.DoesNotExist:
     #     raise Http404()
 
     # Option 2
     course = get_object_or_404(Course, pk=course_id)
-    return render(request, 'single_course.html', {'course': course})
+    return render(request, 'shop/single_course.html', {'course': course})
 
 
 
